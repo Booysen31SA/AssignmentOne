fe50c5d4bd7d5822b080b06225c862f3d62247a6
21600WJK9529 <43211428+21600WJK9529@users.noreply.github.com>
2019-02-13 20:41:40 +0200

    Add files via upload
    Changed GUI look

--- TrafficFine/src/main/java/AssignmentOne/App.java
+++ TrafficFine/src/main/java/AssignmentOne/App.java
@@ -14,6 +14,17 @@ public class App
         //
         // System.out.println( "Hello World!" );
         GUI frame = new GUI();
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
         frame.setVisible(true);
         frame.setSize(400, 200);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
